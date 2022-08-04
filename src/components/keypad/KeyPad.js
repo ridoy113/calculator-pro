13d115d413d61b0cd46d9f87e91849bda045a4d1
ridoy113 <almamounhridoy113@gmail.com>
2022-08-04 18:39:49 +0600

    make keyboard

--- src/components/keypad/KeyPad.js
+++ src/components/keypad/KeyPad.js
@@ -3,9 +3,109 @@ import './KeyPad.css'
 
 
 const KeyPad = () => {
+
+
+
+    const keys = [
+        {
+            keyCode: 55,
+            label: "7",
+        },
+        {
+            keyCode: 56,
+            label: "8",
+        },
+        {
+            keyCode: 57,
+            label: "9",
+        },
+        {
+            keyCode: 52,
+            label: "4",
+        },
+        {
+            keyCode: 53,
+            label: "5",
+        },
+        {
+            keyCode: 54,
+            label: "6",
+        },
+        {
+            keyCode: 49,
+            label: "1",
+        },
+        {
+            keyCode: 50,
+            label: "2",
+        },
+        {
+            keyCode: 51,
+            label: "3",
+        },
+        {
+            keyCode: 48,
+            label: "0",
+        },
+        {
+            keyCode: 190,
+            label: ".",
+        },
+        {
+            keyCode: 13,
+            label: "=",
+        },
+    ];
+
+    const symbols = [
+        {
+            label: "⌫",
+            keyCode: 8,
+            value: "backspace",
+        },
+        {
+            label: "÷",
+            keyCode: 111,
+            value: "/",
+        },
+        {
+            label: "×",
+            keyCode: 56,
+            value: "*",
+        },
+        {
+            label: "﹣",
+            keyCode: 109,
+            value: "-",
+        },
+        {
+            label: "+",
+            keyCode: 107,
+            value: "+",
+        },
+    ];
+
+
+
+
+
+
+
     return (
         <div className='keypad'>
-            <h1>KeyPad</h1>
+            <div className='keypad_keys'>
+                {
+                    keys.map((item, index) => <p key={index}>{item.label}</p>)
+                }
+            </div>
+
+
+
+            <div className='keypad_symbols'>
+                {
+                    symbols.map((item, index) => <p key={index}>{item.label}</p>)
+                }
+            </div>
         </div>
     );
 };

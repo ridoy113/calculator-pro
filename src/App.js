9701a8f898850f9de470b4f4140e1f855579921e
ridoy113 <almamounhridoy113@gmail.com>
2022-08-04 17:35:46 +0600

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
+import { useState } from 'react';
 import './App.css';
+import moonIcon from './assests/moon.png'
+import sunIcon from './assests/sun.png'
+import Header from './components/header/Header';
+import KeyPad from './components/keypad/KeyPad';
+
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false)
   return (
     <div className="app">
       <div className='app_calculator'>
         <div className='app_calculator_navbar'>
-          <div className='app_calculator_navbar_toggle'>
-            <div className='app_calculator_navbar_toggle_circle'/>
+          <div
+            className='app_calculator_navbar_toggle'
+            onClick={() => setIsDarkMode(!isDarkMode)}
+          >
+            <div className={
+              `app_calculator_navbar_toggle_circle 
+            ${isDarkMode ?
+                "app_calculator_navbar_toggle_circle_active" : ""}`
+            } />
           </div>
-          <img src="" alt="" />
+          <img src={isDarkMode ? moonIcon : sunIcon} alt="" />
         </div>
+
+        <Header />
+        <KeyPad />
       </div>
     </div>
   );
---
+++ src/components/keypad/KeyPad.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './KeyPad.css'
+
+
+const KeyPad = () => {
+    return (
+        <div className='keypad'>
+            <h1>KeyPad</h1>
+        </div>
+    );
+};
+
+export default KeyPad;
